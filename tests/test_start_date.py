16f4c5b3b6806f7628b37f5c02408efd356d043e
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-01-08 12:02:25 +0530

    fix integration tests

--- tests/test_start_date.py
+++ tests/test_start_date.py
@@ -13,9 +13,8 @@ class Amazon_AdsStartDateTest(StartDateTest, Amazon_AdsBaseTest):
 
     def streams_to_test(self):
         streams_to_exclude = {
-            #this needs to check as primary key is missing
-            'sponsored_brands_store_assets',
             # No data available for streams
+            'sponsored_brands_store_assets',
             'sponsored_brands_negative_keywords',
             'sponsored_display_brand_safety_list','sponsored_display_negative_targeting_clauses','sponsored_brands_campaigns',
             'sponsored_brands_bid_recommendations','sponsored_brands_campaigns_budget_rules', 'sponsored_brands_ad_groups', 'sponsored_brands_keywords',
@@ -24,7 +23,7 @@ class Amazon_AdsStartDateTest(StartDateTest, Amazon_AdsBaseTest):
             'sponsored_products_ad_groups','sponsored_products_keywords', 'sponsored_products_negative_keywords',
             'sponsored_products_ads', 'invoices', 'sponsored_brands_budget_rules', 'sponsored_products_budget_rules',
             'sponsored_display_campaigns', 'sponsored_display_product_ads', 'sponsored_display_ad_groups',
-            'sponsored_display_targetings', 'sponsored_display_campaigns_budget_rules'}
+            'sponsored_display_targetings', 'sponsored_display_campaigns_budget_rules', 'sponsored_display_creatives', 'profiles', 'portfolios', 'sponsored_display_budget_rules_campaigns', 'sponsored_display_budget_rules_campaigns'}
         return self.expected_stream_names().difference(streams_to_exclude)
 
     @property
@@ -32,4 +31,4 @@ class Amazon_AdsStartDateTest(StartDateTest, Amazon_AdsBaseTest):
         return "2019-03-25T00:00:00Z"
     @property
     def start_date_2(self):
-        return "2025-01-25T00:00:00Z"
+        return "2025-07-16T05:24:30.00Z"
