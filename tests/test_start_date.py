0e81a7d5f289291eb1903ca963ec4df61ac330e0
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-01-08 11:51:37 +0530

    fix integration tests

--- tests/test_start_date.py
+++ tests/test_start_date.py
@@ -1,1 +1,35 @@
-sponsored_display_campaigns_budget_rules
+from base import Amazon_AdsBaseTest
+from tap_tester.base_suite_tests.start_date_test import StartDateTest
+
+
+
+class Amazon_AdsStartDateTest(StartDateTest, Amazon_AdsBaseTest):
+    """Instantiate start date according to the desired data set and run the
+    test."""
+
+    @staticmethod
+    def name():
+        return "tap_tester_amazon_ads_start_date_test"
+
+    def streams_to_test(self):
+        streams_to_exclude = {
+            #this needs to check as primary key is missing
+            'sponsored_brands_store_assets',
+            # No data available for streams
+            'sponsored_brands_negative_keywords',
+            'sponsored_display_brand_safety_list','sponsored_display_negative_targeting_clauses','sponsored_brands_campaigns',
+            'sponsored_brands_bid_recommendations','sponsored_brands_campaigns_budget_rules', 'sponsored_brands_ad_groups', 'sponsored_brands_keywords',
+            'sponsored_brands_negative_targets', 'sponsored_brands_product_targets','sponsored_brands_ads',
+            'sponsored_brands_ad_creatives', 'sponsored_brands_budget_rules_campaigns','sponsored_products_campaigns',
+            'sponsored_products_ad_groups','sponsored_products_keywords', 'sponsored_products_negative_keywords',
+            'sponsored_products_ads', 'invoices', 'sponsored_brands_budget_rules', 'sponsored_products_budget_rules',
+            'sponsored_display_campaigns', 'sponsored_display_product_ads', 'sponsored_display_ad_groups',
+            'sponsored_display_targetings', 'sponsored_display_campaigns_budget_rules'}
+        return self.expected_stream_names().difference(streams_to_exclude)
+
+    @property
+    def start_date_1(self):
+        return "2019-03-25T00:00:00Z"
+    @property
+    def start_date_2(self):
+        return "2025-01-25T00:00:00Z"
